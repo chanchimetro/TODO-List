7b34381b2268c612b63911585f101a7cf624417b
chanchimetro <48773442+chanchimetro@users.noreply.github.com>
2023-03-29 17:25:02 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ function agregarItem()
         id: idUnico,
         titulo: document.getElementById('task').value,
         hecho: false,
-        fecha: Date.now()
+        fechaCreacion: Date.now()
     }
     idUnico++;
     itemList.push(i);
@@ -18,6 +18,15 @@ function mostrarLista()
 {
     document.getElementById('lista').innerHTML = "";
     for (let index = 0; index < itemList.length; index++) {
-        document.getElementById('lista').innerHTML += `<li id="${itemList[index].id}">${itemList[index].titulo} <button onclick = "borrarItem"></button></li>`;
+        document.getElementById('lista').innerHTML += `<li id="${itemList[index].id}">${itemList[index].titulo} <button onclick = "borrarItem(${itemList[index].id})">X</button> <button onclick = "tacharItem(${itemList[index].id})">✓</button></li>`;
+        if (itemList[index].hecho) {
+            document.getElementById(`${itemList[index].id}`).style.textDecoration = "line-through";
+        }
     }
+}
+
+function tacharItem(id) {
+    let index = itemList.findIndex((item) => item.id == id);
+    itemList[index].hecho = true;
+    mostrarLista();
 }
