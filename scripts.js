27a3b4db24ed76f760ab67300f8a9d8f8fec802e
chanchimetro <48773442+chanchimetro@users.noreply.github.com>
2023-03-29 11:57:12 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+let itemList = [];
+let idUnico = 0;
+
+function agregarItem()
+{
+    var i = {
+        id: idUnico,
+        titulo: document.getElementById('task').value,
+        hecho: false,
+        fecha: Date.now()
+    }
+    idUnico++;
+    itemList.push(i);
+    mostrarLista();
+}
+
+function mostrarLista()
+{
+    document.getElementById('lista').innerHTML = "";
+    for (let index = 0; index < itemList.length; index++) {
+        document.getElementById('lista').innerHTML += '<li></li>';
+    }
+}
